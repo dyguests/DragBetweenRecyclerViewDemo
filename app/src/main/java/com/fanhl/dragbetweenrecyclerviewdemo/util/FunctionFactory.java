815766f2c54c130d2e5090b63d05c1f1421ef2ab
fanhl <fanhonglin@klicen.com>
2017-04-10 17:42:26 +0800

    factory

--- app/src/main/java/com/fanhl/dragbetweenrecyclerviewdemo/util/FunctionFactory.java
+++ app/src/main/java/com/fanhl/dragbetweenrecyclerviewdemo/util/FunctionFactory.java
@@ -1,8 +1,24 @@
 package com.fanhl.dragbetweenrecyclerviewdemo.util;
 
+import com.fanhl.dragbetweenrecyclerviewdemo.model.MainModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by fanhl on 2017/4/10.
  */
 
 public class FunctionFactory {
+    private static final List<MainModel.FunctionItemWrap> serviceFunctions;
+    private static final List<MainModel.FunctionItemWrap> securityFunctions;
+    private static final List<MainModel.FunctionItemWrap> toolsFunctions;
+
+    static {
+        serviceFunctions = new ArrayList<>();
+        securityFunctions = new ArrayList<>();
+        toolsFunctions = new ArrayList<>();
+    }
+
+
 }
